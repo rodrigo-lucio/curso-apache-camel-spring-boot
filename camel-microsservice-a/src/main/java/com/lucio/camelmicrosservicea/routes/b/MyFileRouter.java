2ded042c5c90000306b078d33ef056a75a9cc4d0
Rodrigo Lúcio <luciodigo@gmail.com>
2023-01-10 21:59:36 -0300

    Criando condições com o choice e utilizando variáveis

--- camel-microsservice-a/src/main/java/com/lucio/camelmicrosservicea/routes/b/MyFileRouter.java
+++ camel-microsservice-a/src/main/java/com/lucio/camelmicrosservicea/routes/b/MyFileRouter.java
@@ -10,8 +10,26 @@ public class MyFileRouter extends RouteBuilder {
     public void configure() throws Exception {
 
         from("file:files/input")
-        .log("${body}")
-        .to("file:files/output"); 
+        //.pipeline()
+        .routeId("Files-Input-Route")
+        .transform().body(String.class)
+        .choice() //Content Based Routing
+            .when(simple("${file:ext} == 'xml'"))
+                .log("XML FILE")
+            .when(simple("${body} contains 'USD'"))
+                .log("Not an XML FILE BUT contains USD")
+            .otherwise()
+                .log("Not an XML FILE")
+        .end()
+        .to("direct://log-file-values") //Direciona para o from da linha 29, onde vai jogando as informações dos arquivos
+        .to("file:files/output"); //Joga para a pasta files/output
+        
+        from("direct:log-file-values") //Foi direcionado da parte anterior na linha 26
+        .log("${messageHistory} ${file:absolute.path}")
+        .log("${file:name} ${file:name.ext} ${file:name.noext} ${file:onlyname}")
+        .log("${file:onlyname.noext} ${file:parent} ${file:path} ${file:absolute}") 
+        .log("${file:size} ${file:modified}")
+        .log("${routeId} ${camelId} ${body}");
                 
         
     }
