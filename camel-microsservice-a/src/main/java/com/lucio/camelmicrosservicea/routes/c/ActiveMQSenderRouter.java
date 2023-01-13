f34863785025ae76305380b2a90ec8ad8749b1f7
Rodrigo Lúcio <luciodigo@gmail.com>
2023-01-13 18:18:59 -0300

    Criptografando e descriptografando mensagens

--- camel-microsservice-a/src/main/java/com/lucio/camelmicrosservicea/routes/c/ActiveMQSenderRouter.java
+++ camel-microsservice-a/src/main/java/com/lucio/camelmicrosservicea/routes/c/ActiveMQSenderRouter.java
@@ -1,6 +1,15 @@
 package com.lucio.camelmicrosservicea.routes.c;
 
+import java.io.IOException;
+import java.security.Key;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.converter.crypto.CryptoDataFormat;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,6 +29,7 @@ public class ActiveMQSenderRouter extends RouteBuilder {
         //Agora envia o json da pasta para o ActiveMQ
         from("file:files/json")
         .log("${body}")
+        .marshal(createEncryptor())
         .to("activemq:my-activemq-queue");
         
         //Tambem xml
@@ -28,4 +38,14 @@ public class ActiveMQSenderRouter extends RouteBuilder {
         .to("activemq:my-activemq-xml-queue");
     }
 
+    private CryptoDataFormat createEncryptor() throws KeyStoreException, IOException, NoSuchAlgorithmException, CertificateException, UnrecoverableKeyException {
+        KeyStore keyStore = KeyStore.getInstance("JCEKS");
+        ClassLoader classLoader = getClass().getClassLoader();
+        keyStore.load(classLoader.getResourceAsStream("myDesKey.jceks"), "someKeystorePassword".toCharArray());
+        Key sharedKey = keyStore.getKey("myDesKey", "someKeyPassword".toCharArray());
+
+        CryptoDataFormat sharedKeyCrypto = new CryptoDataFormat("DES", sharedKey);
+        return sharedKeyCrypto;
+    }
+    
 }
